24228f0a86f7e5b0ccb1e0c04a1aa6079df14c9b
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-07-28 15:02:44 +0200

    icon styles applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ContextType, useState } from "react";
+import React from "react";
 import Header from "./components/Header/Header";
 import Deck from "./components/card_deck/deck/Deck";
 
--- src/components/card_deck/card/card.tsx
+++ src/components/card_deck/card/card.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import {
-  FontAwesomeIcon,
-  FontAwesomeIconProps,
-} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./card.module.css";
 import Icons from "./icons/icons";
 
